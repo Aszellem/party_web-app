fb68786a8eb39385ce6982675da0e67a8bc2c0b9
Aszellem <aszellemek@gmail.com>
2022-02-11 11:15:31 +0100

    0.1

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const mysql = require('mysql');
+const cors=require('cors');
+app.use(cors());
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: '',
+    database: 'rendteszt'});
+
+app.route("/kezdolap")
+    .get(function(req,res){
+        const q="SELECT r.rend_id AS id, r.rend_nev AS 'rend_nev', date_format(r.idopont, '%Y %M %d') AS 'idopont', h.helyszin_nev AS 'helyszin_nev', e.eloado_nev AS 'eloado_nev'"
+        +" FROM helyszin AS h INNER JOIN"
+             +" rendezveny AS r"
+             +" ON h.helyszin_id = r.helyszin_id INNER JOIN"
+             +" rend_eloado AS re"
+             +" ON r.rend_id = re.rend_id INNER JOIN"
+             +" eloado AS e"
+             +" ON re.eloado_id = e.eloado_id"
+             +" ORDER BY r.idopont DESC;";        
+        pool.query(q, function(error,results){
+            if(!error){
+                res.send(results);
+            }else{
+                res.send(error);
+            }
+        })
+    });
+
+app.route("/kezdolap/:id")
+    .get(function(req,res){
+        const q="SELECT r.rend_id, r.rend_nev, date_format(r.idopont, '%Y %M %d') AS 'idopont', r.kategoria, r.korosztaly, r.leiras, r.ar, h.helyszin_nev, e.eloado_nev "+
+        " FROM helyszin AS h JOIN"+
+             " rendezveny AS r"+
+             " ON h.helyszin_id = r.helyszin_id JOIN"+
+             " rend_eloado AS re"+
+             " ON r.rend_id = re.rend_id JOIN"+
+             " eloado AS e"+
+             " ON re.eloado_id = e.eloado_id"+
+             " WHERE r.rend_id = ?";
+        pool.query(q, [req.params.id],
+            function(error, results){
+            if(!error){
+                res.send(results);
+            }else{
+                res.send(error);
+            }
+        })
+    })
+
+
+
+    
+
+    
+
+app.listen(3000, function(){
+    console.log("Szerver - 3000 - elindítva")
+})
---
+++ client/index.js
@@ -0,0 +1,51 @@
+let id;
+
+
+function kezdolap(){
+    const url = 'http://localhost:3000/kezdolap/';
+    const lista = document.getElementById("rendezvenyek");
+    fetch(url)
+        .then((response) => response.json())
+        .then(json => {
+            lista.innerHTML = "";
+            json.forEach(f => {
+                id=f.id;
+                lista.innerHTML +=
+                "<div class='card' id='"+id+"'>"+
+                "<p>"+f.rend_nev+"</p>"+
+                "<p>"+f.idopont+"</p>"+
+                "<p>"+f.eloado_nev+"</p>"+
+                "<p>"+f.helyszin_nev+"</p>"+
+                "<button type='button' class='button' id='button"+id+"' onClick='bovebben("+id+")' style='vertical-align:middle;'><span><strong>Bővebben</strong></span></button>"+
+                "</div>"+
+                "</div>";
+            })
+        })
+        .catch(err => console.log(err));
+}
+
+function bovebben(id){
+        const url='http://localhost:3000/kezdolap/'+id;
+        const lista=document.getElementById("rendezvenyek");
+        lista.innerHTML="";
+        fetch(url)
+            .then((response) => response.json())
+            .then(json => {
+                console.log(json),
+                lista.innerHTML="";
+                lista.innerHTML+=
+                "<div class='card col-sm-4' >"+
+                "<p><h2>"+json[0].rend_nev+"</h2></p>"+
+                "<p>Időpont: "+json[0].idopont+"</p>"+
+                "<p>Fellépők: "+json[0].eloado_nev+"</p>"+
+                "<p>Helyszín: "+json[0].helyszin_nev+"</p>"+
+                "<p>Kategória: "+json[0].kategoria+"</p>"+
+                "<p id='leiras'>Esemény leírása: "+json[0].leiras+"</p>"+
+                "<p>Ár: "+json[0].ar+"</p>"+
+                "</div>";
+        })
+            .catch((err) => console.log(err));
+}
+
+kezdolap();
+
