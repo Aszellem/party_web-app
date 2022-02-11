85bd05eb64c516c1306f922c4fde08a10b981c6e
Aszellem <aszellemek@gmail.com>
2022-02-11 11:51:44 +0100

    0.2 - emoji

--- client/index.js
+++ client/index.js
@@ -13,9 +13,9 @@ function kezdolap(){
                 lista.innerHTML +=
                 "<div class='card' id='"+id+"'>"+
                 "<p>"+f.rend_nev+"</p>"+
-                "<p>"+f.idopont+"</p>"+
-                "<p>"+f.eloado_nev+"</p>"+
-                "<p>"+f.helyszin_nev+"</p>"+
+                "<p>&#9201;"+ f.idopont+"</p>"+
+                "<p>&#127913;"+ f.eloado_nev+"</p>"+
+                "<p>&#128205"+f.helyszin_nev+"</p>"+
                 "<button type='button' class='button' id='button"+id+"' onClick='bovebben("+id+")' style='vertical-align:middle;'><span><strong>Bővebben</strong></span></button>"+
                 "</div>"+
                 "</div>";
